6a61983609f41fb8d5d729652e936bb0802604ab
byteful <imfromwikipedia@gmail.com>
2021-08-01 13:51:00 -0500

    Modified build.gradle to shade MongoDB

--- src/main/java/de/zoom/opskyblock/OPSkyblockPlugin.java
+++ src/main/java/de/zoom/opskyblock/OPSkyblockPlugin.java
@@ -1,9 +1,7 @@
 package de.zoom.opskyblock;
 
-import me.lucko.helper.maven.MavenLibrary;
 import me.lucko.helper.plugin.ExtendedJavaPlugin;
 
-@MavenLibrary(groupId = "org.mongodb", artifactId = "mongo-java-driver", version = "3.12.9")
 public class OPSkyblockPlugin extends ExtendedJavaPlugin { // that class is an abstract class, which is sort of like an interface, except mixed with a class
     private static OPSkyblockPlugin instance;
 
